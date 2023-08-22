ab40d6e5b0b655b16f8ab123739d8b9cc8e67fad
MChinnam412 <138657062+MChinnam412@users.noreply.github.com>
2023-08-22 11:15:46 +0530

    Update mysql_utils.py

--- src/utils/mysql_utils.py
+++ src/utils/mysql_utils.py
@@ -24,17 +24,26 @@ class MySQLConnection:
         connecting to MySQL database
         """
         try:
+            
             self.mydb = mysql.connector.connect(
-                host=config.get("MYSQL_HOST", str, None),
-                port=config.get("MYSQL_PORT", int, None),
-                database=config.get("MYSQL_DB", str, None),
-                user=config.get("MYSQL_USER", str, None),
-                password=config.get("MYSQL_PASSWORD", str, None)
+                host=os.environ.get("MYSQL_HOST"),
+                port=int(os.environ.get("MYSQL_PORT")),
+                database=os.environ.get("MYSQL_DB"),
+                user=os.environ.get("MYSQL_USER"),
+                password=os.environ.get("MYSQL_PASSWORD"),
             )
-            self.table_name = config.get("MYSQL_TABLE_NAME", str, None)
+            self.table_name = os.environ.get("MYSQL_TABLE_NAME")
             self.cursor = self.mydb.cursor()
             logging.info("Successfully connected to MySQL")
         except Exception as ex:
+            if config.get("MYSQL_HOST") is None:
+                config.get("MYSQL_HOST")
+            if config.get("MYSQL_PORT") is None:
+                print("MYSQL_PORT none")
+            if config.get("MYSQL_DB") is None:
+                print("MYSQL_DB")
+            if config.get("MYSQL_PASSWORD") is None:
+                print("MYSQL_PASSWORD")
             logging.error(f"Error connecting to MySQL: {ex}")
 
     def get_conversation_data(self, unique_id):
@@ -44,7 +53,6 @@ class MySQLConnection:
         :return:
         """
         try:
-            # query_table = self.cursor.execute(f"select conversation from chat_test_1 where sessionId = '{unique_id}'")
             query_table = f"select conversation from {self.table_name} where sessionId = %s ;"
             self.cursor.execute(query_table, [unique_id])
             output = []
@@ -100,3 +108,4 @@ class MySQLConnection:
             logging.info("Successfully closed connection to MySQL")
         except Exception as ex:
             logging.error(f"Error closing connection to MySQL: {ex}")
+
