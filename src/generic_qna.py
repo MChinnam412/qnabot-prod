522e719a8b316c6a2b16cde3c4979172c1c3ecba
MChinnam412 <138657062+MChinnam412@users.noreply.github.com>
2023-10-10 12:56:09 +0530

    Update generic_qna.py

--- src/generic_qna.py
+++ src/generic_qna.py
@@ -2,13 +2,15 @@ import pickle
 from datetime import datetime
 
 from langchain.memory import ConversationBufferMemory
+from starlette.config import Config
 
 from src.__init__ import DEFAULT_GENERIC_OUTPUT, URLS
 from src.azure_qna_maker import QuestionAnswering
 from src.openaI_code import OpenAIQuestionAnswering
 from src.utils.logger_utils import Logger
 from src.utils.mysql_utils import MySQLConnection
-import os
+
+config = Config("configs/properties.conf")
 
 logger = Logger()
 logging = logger.get_logger()
@@ -30,10 +32,9 @@ class GenericQnaAnswering:
         loading OpenAI, Azure QnA Maker, MySQL connections
         """
         try:
-            # To use OpenAI uncomment these lines
-            # self.openai_instance = OpenAIQuestionAnswering(URLS)
+            # self.openai_instance = OpenAIQuestionAnswering(URLS[:1])
             # self.openai_instance.load_chain()
-            self.azure_qna_maker = QuestionAnswering()
+            self.azure_qna_maker = QuestionAnswering(config.get('AZURE_QNA_MAKER_CONFIDENCE', float, 0.5))
             self.mysql_instance = MySQLConnection()
             logging.info("Successfully initialized OpenAI")
         except Exception as ex:
@@ -50,7 +51,7 @@ class GenericQnaAnswering:
             output = self.mysql_instance.get_conversation_data(sessionId)
             return output
         except Exception as ex:
-            logging.info(f"Error while checking if session exists: {ex}")
+            logging.error(f"Error while checking if session exists: {ex}")
         return None
 
     def get_current_timestamp(self):
@@ -63,7 +64,49 @@ class GenericQnaAnswering:
             self.current_timestamp = datetime.now().strftime("%m/%d/%YT%H:%M:%S.%f")
             self.mysql_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         except Exception as ex:
-            print(f"Error while generating timestamp: {ex}")
+            logging.error(f"Error while generating timestamp: {ex}")
+
+    def convert_to_html(self, data):
+        """
+        converting output from QnA Maker to html
+        :param sessionId:
+        :return:
+        """
+        try:
+            data = data.strip()
+            split_data = data.split("\n")
+            output_data = []
+            is_list = False
+            for d in split_data:
+                d = d.strip()
+                if len(d) > 0:
+                    if "* " in d:
+                        # Checking for list
+                        d = d.replace("*", "")
+                        d = d.strip()
+                        d = "<li>" + d + "</li>"
+                        if not is_list:
+                            # start of list
+                            d = "<ul>" + d
+                            is_list = True
+                    elif is_list:
+                        # end of list
+                        output_data[-1] = output_data[-1] + "</ul>"
+                        is_list = False
+                    if len(d) > 0:
+                        if ":" in d and ("More Information" in d or "Link" in d):
+                            # checking for Source urls
+                            temp = d.split(":")
+                            o = f'<a href="{":".join(temp[1:]).strip()}">{temp[0].strip()}</a><br><br>'
+                            d = o
+                        if "<" not in d:
+                            d += "<br>"
+                        output_data.append(d)
+            output_data[-1] = output_data[-1].replace("<br>", "")
+            return "".join(output_data)
+        except Exception as ex:
+            logging.error(f"Error while converting to html: {ex}")
+        return data
 
     def get_answer(self, sessionId, question):
         """
@@ -87,15 +130,16 @@ class GenericQnaAnswering:
                 join_question = "".join(question.split())
                 sessionId = str(hash(self.current_timestamp + join_question))
             output = self.azure_qna_maker.get_output(question)
-
-            # To use Open AI Un-comment these lines
             # if output["answer"] == "No answer found":
-                # output = self.openai_instance.query_data(question)
+            # output = self.openai_instance.query_data(question)
             output["sessionId"] = sessionId
             output["timestamp"] = self.current_timestamp
 
+            # convert the output data to html
+            output["answer"] = self.convert_to_html(output["answer"])
+
             memory.save_context({"input": question}, {"output": output["answer"]})
-            #converting the memory to pickle to save in mysql
+            # converting the memory to pickle to save in mysql
             bin_pickle = pickle.dumps(memory)
             if self.sessionId_exists:
                 # If sessionId exists only updating the timestamp and ConversationBufferMemory
@@ -104,7 +148,5 @@ class GenericQnaAnswering:
                 self.mysql_instance.insert_data([sessionId, self.mysql_timestamp, self.mysql_timestamp, bin_pickle])
             return output
         except Exception as ex:
-            logging.info("Error while running output for QnA Maker & OpenAI")
+            logging.error(f"Error while running output for QnA Maker & OpenAI: {ex}")
         return DEFAULT_GENERIC_OUTPUT
-
-
